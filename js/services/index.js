550234de76a395cf1713e00c33d0297c65695693
Carlos <nicolaugalves@gmail.com>
2026-02-02 11:08:58 -0300

    WIP testing website but seems good for me

---
+++ js/services/index.js
@@ -0,0 +1,9 @@
+/**
+ * Services Index
+ * Re-export all services for easier imports
+ */
+
+export * from './auth.service.js';
+export * from './product.service.js';
+export * from './order.service.js';
+export * from './cart.service.js';
